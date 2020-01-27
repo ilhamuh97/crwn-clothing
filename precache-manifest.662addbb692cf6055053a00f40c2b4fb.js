2d981f20cc150e19e87a8d2adb9c3009f74b0e96
ilham muhammad <ilhamuh97@gmail.com>
2020-01-27 17:56:23 +0100

    Updates

---
+++ precache-manifest.662addbb692cf6055053a00f40c2b4fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0efd070986ddfccfdab2bfc74eff0e2f",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "b5613106bde7d56c2127",
+    "url": "/crwn-clothing/static/css/main.f4a2f565.chunk.css"
+  },
+  {
+    "revision": "bc2117143881cfc03d10",
+    "url": "/crwn-clothing/static/js/2.b7209063.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/crwn-clothing/static/js/2.b7209063.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5613106bde7d56c2127",
+    "url": "/crwn-clothing/static/js/main.6c3189e3.chunk.js"
+  },
+  {
+    "revision": "0476b8ec577c2ae33b1e",
+    "url": "/crwn-clothing/static/js/runtime-main.844656b4.js"
+  }
+]);
