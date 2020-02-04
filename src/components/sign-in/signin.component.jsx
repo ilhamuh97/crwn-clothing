952cd19769b0c4a05ba222bad3ffe607316305ad
ilham muhammad <ilhamuh97@gmail.com>
2020-02-04 16:07:42 +0100

    create form-input component and style it

--- src/components/sign-in/signin.component.jsx
+++ src/components/sign-in/signin.component.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import "./signin.styles.scss"
 
+import FormInput from '../form-in/form-in.component'
+
 class SignIn extends React.Component{
     constructor(props){
         super(props)
@@ -31,21 +33,21 @@ class SignIn extends React.Component{
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
                 <form onSubmit={this.handleSubmit}>
-                    <input 
+                    <FormInput 
                     name="email" 
                     type="email" 
                     value={this.state.email} 
-                    onChange={this.handleChange} 
+                    handleChange={this.handleChange} 
+                    label = 'Email'
                     required />
-                    <label>Email</label>
 
-                    <input 
+                    <FormInput 
                     name="password" 
                     type="password" 
                     value={this.state.password}
-                    onChange={this.handleChange} 
+                    handleChange={this.handleChange} 
+                    label = 'Password'
                     required />
-                    <label>Password</label>
 
                     <input type="submit" value="Submit Form"></input>
                 </form>
